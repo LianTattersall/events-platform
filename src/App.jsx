563cf62c63d9b23aaf90d21026b6872a41850903
Lian Tattersall <liantattersall@Lians-MBP.lan>
2025-01-31 11:31:07 +0000

    add routing and initial styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router";
 import Home from "./Pages/Home";
+import { MenuDrawerProvider } from "./Contexts/MenuDrawContext";
+import Navbar from "./Components/Navbar";
+import MenuDrawer from "./Components/MenuDrawer";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <MenuDrawerProvider>
+      <Navbar />
+      <MenuDrawer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </MenuDrawerProvider>
   );
 }
 
