a8b14b2ca40297722f431249b84e7d92e239ce14
Lian Tattersall <lian.tattersall.20@ucl.ac.uk>
2025-02-03 12:11:02 +0000

    add change to close menu by tapping on the page and minor styling changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,15 @@ import { useContext } from "react";
 import { MenuDrawerContext } from "../Contexts/MenuDrawContext";
 
 export default function Home() {
-  const { menuDrawerOpen } = useContext(MenuDrawerContext);
+  const { menuDrawerOpen, setMenuDrawerOpen } = useContext(MenuDrawerContext);
 
   return (
     <>
       <div
         className={menuDrawerOpen ? "margin-with-drawer" : "margin-no-drawer"}
+        onClick={() => {
+          setMenuDrawerOpen(false);
+        }}
       >
         <h1>home</h1>
       </div>
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -3,14 +3,19 @@ import { MenuDrawerContext } from "../Contexts/MenuDrawContext";
 import SignUpForm from "../Components/SignUpForm";
 
 export default function SignUp() {
-  const { menuDrawerOpen } = useContext(MenuDrawerContext);
+  const { menuDrawerOpen, setMenuDrawerOpen } = useContext(MenuDrawerContext);
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [admin, setAdmin] = useState(null);
 
   return (
-    <div className={menuDrawerOpen ? "margin-with-drawer" : "margin-no-drawer"}>
+    <div
+      className={menuDrawerOpen ? "margin-with-drawer" : "margin-no-drawer"}
+      onClick={() => {
+        setMenuDrawerOpen(false);
+      }}
+    >
       <SignUpForm />
     </div>
   );
