3a2dc05044c92744454fcf1c4a41a67170c5af1d
Lian Tattersall <lianwan468@gmail.com>
2025-02-04 15:35:10 +0000

    add access token to userContext

--- src/Contexts/UserContext.jsx
+++ src/Contexts/UserContext.jsx
@@ -6,9 +6,19 @@ export const UserContextProvider = ({ children }) => {
   const [userId, setUserId] = useState(localStorage.getItem("user_id"));
   const [admin, setAdmin] = useState(localStorage.getItem("admin"));
   const [name, setName] = useState(localStorage.getItem("name"));
+  const [accessTocken, setAccessToken] = useState("");
   return (
     <UserContext.Provider
-      value={{ userId, setUserId, admin, setAdmin, name, setName }}
+      value={{
+        userId,
+        setUserId,
+        admin,
+        setAdmin,
+        name,
+        setName,
+        accessTocken,
+        setAccessToken,
+      }}
     >
       {children}
     </UserContext.Provider>
