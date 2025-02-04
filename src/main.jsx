3b62df6b596ac6e41f0928ede14a568dc51b9439
Lian Tattersall <lianwan468@gmail.com>
2025-02-04 15:35:44 +0000

    add google provider to app

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+let googleClientId = "";
+async function getEnv() {
+  googleClientId = await import.meta.env.VITE_GOOGLE_CLIENT_ID;
+}
+await getEnv();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <GoogleOAuthProvider clientId={googleClientId}>
+        <App />
+      </GoogleOAuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
