39edee74161159b00cf5c1bac49d348259a2a2d6
Lian Tattersall <lianwan468@gmail.com>
2025-02-05 12:21:52 +0000

    add google login button to login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { MenuDrawerContext } from "../Contexts/MenuDrawContext";
 import LoginForm from "../Components/LoginForm";
+import LoginGoogle from "../Components/LoginGoogle";
 
 export default function Login() {
   const { menuDrawerOpen, setMenuDrawerOpen } = useContext(MenuDrawerContext);
+
   return (
     <div
       className={menuDrawerOpen ? "margin-with-drawer" : "margin-no-drawer"}
@@ -12,6 +14,7 @@ export default function Login() {
       }}
     >
       <LoginForm />
+      <LoginGoogle />
     </div>
   );
 }
