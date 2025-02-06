d668f22223c2dab4faecaf82fa1b8b3f0f8a14f7
Lian Tattersall <lianwan468@gmail.com>
2025-02-06 16:20:13 +0000

    add buttons for signing up saving and adding to google cal to event page

--- src/Pages/EventPage.jsx
+++ src/Pages/EventPage.jsx
@@ -2,17 +2,58 @@ import { useContext, useEffect, useState } from "react";
 import { MenuDrawerContext } from "../Contexts/MenuDrawContext";
 import { useParams } from "react-router";
 import { getEventById } from "../api";
+import AddToGoogleCal from "../Components/AddToGoogleCal";
+import SignUpButton from "../Components/SignUpButton";
+import SaveButton from "../Components/SaveButton";
 
 export default function EventPage() {
-  const { menuDrawerOpen } = useContext(MenuDrawerContext);
+  const { menuDrawerOpen, setMenuDrawerOpen } = useContext(MenuDrawerContext);
+
+  const [loading, setLoading] = useState(true);
   const [event, setEvent] = useState({});
+  const [error, setError] = useState("");
+
   const { event_id } = useParams();
 
   useEffect(() => {
-    getEventById(event_id).then(({ event }) => {
-      setEvent(event);
-    });
+    getEventById(event_id)
+      .then(({ event }) => {
+        setEvent(event);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        if (err.response.status == 404) {
+          setError("404 - Page not found");
+        }
+      });
   }, []);
+
+  if (loading) {
+    return (
+      <div
+        className={menuDrawerOpen ? "margin-with-drawer" : "margin-no-drawer"}
+        onClick={() => {
+          setMenuDrawerOpen(false);
+        }}
+      >
+        <p>Loading Event details</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div
+        className={menuDrawerOpen ? "margin-with-drawer" : "margin-no-drawer"}
+        onClick={() => {
+          setMenuDrawerOpen(false);
+        }}
+      >
+        <p className="error">{error}</p>
+      </div>
+    );
+  }
 
   return (
     <div
@@ -22,13 +63,23 @@ export default function EventPage() {
       }}
     >
       <h1>{event.event_name}</h1>
-      <img src={event.image_URL} style={{ height: "400px" }}></img>
+      <img
+        src={event.image_URL}
+        style={{ width: "100%", maxWidth: "500px" }}
+      ></img>
       <p>{event.description}</p>
       <p>Date: {event.event_date}</p>
       <p>Price: {event.price > 0 ? `£${event.price}` : "Free"}</p>
       <p>
         Location: {event.firstline_address} {event.postcode}
       </p>
+      <AddToGoogleCal
+        eventName={event.event_name}
+        date={event.event_date}
+        dateTime={event.event_date + "T" + event.start_time + "Z"}
+      />
+      <SignUpButton event_id={Number(event_id)} />
+      <SaveButton event_id={event_id} />
     </div>
   );
 }
