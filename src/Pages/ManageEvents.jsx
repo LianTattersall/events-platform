00a65bd8815ca006884ebf10a77d46ee31821c56
Lian Tattersall <lianwan468@gmail.com>
2025-02-10 18:42:53 +0000

    add managing events page

--- src/Pages/ManageEvents.jsx
+++ src/Pages/ManageEvents.jsx
@@ -1,14 +1,93 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { MenuDrawerContext } from "../Contexts/MenuDrawContext";
+import { getEventsOrganisedByUser } from "../api";
+import { UserContext } from "../Contexts/UserContext";
+import { Link } from "react-router";
 
 export default function ManageEvents() {
   const { menuDrawerOpen, setMenuDrawerOpen } = useContext(MenuDrawerContext);
+  const { userId } = useContext(UserContext);
+
+  const [loading, setLoading] = useState(true);
+  const [events, setEvents] = useState([]);
+  const [pastEvents, setPastEvents] = useState([]);
+
+  useEffect(() => {
+    getEventsOrganisedByUser(userId, "curr")
+      .then(({ events }) => {
+        setEvents(events);
+        return getEventsOrganisedByUser(userId, "past");
+      })
+      .then(({ events }) => {
+        setPastEvents(events);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <div
+        className={menuDrawerOpen ? "margin-with-drawer" : "margin-no-drawer"}
+        onClick={() => {
+          setMenuDrawerOpen(false);
+        }}
+      >
+        <p>Loading your events</p>
+      </div>
+    );
+  }
   return (
     <div
       className={menuDrawerOpen ? "margin-with-drawer" : "margin-no-drawer"}
       onClick={() => {
         setMenuDrawerOpen(false);
       }}
-    ></div>
+    >
+      <table>
+        <thead>
+          <tr>
+            <td>Event Name</td>
+            <td>event date</td>
+            <td>Number of signups</td>
+          </tr>
+        </thead>
+        <tbody>
+          {events.map((event) => (
+            <tr>
+              <td>
+                <Link to={`/manageEvents/details/${event.event_id}`}>
+                  {event.event_name}
+                </Link>
+              </td>
+              <td>{event.event_date}</td>
+              <td>{event.signups}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <p>Completed Events</p>
+      <table>
+        <thead>
+          <tr>
+            <td>Event Name</td>
+            <td>event date</td>
+            <td>Number of signups</td>
+          </tr>
+        </thead>
+        <tbody>
+          {pastEvents.map((event) => (
+            <tr>
+              <td>
+                <Link to={`/manageEvents/details/${event.event_id}`}>
+                  {event.event_name}
+                </Link>
+              </td>
+              <td>{event.event_date}</td>
+              <td>{event.signups}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
