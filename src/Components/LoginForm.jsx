8c07ac465984381edc68bfac960d81c17361a9d2
Lian Tattersall <lianwan468@gmail.com>
2025-02-12 17:14:25 +0000

    add styling and extra errror case

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -4,12 +4,13 @@ import { auth } from "../../firebase";
 import { getUser } from "../api";
 import { useNavigate } from "react-router";
 import { UserContext } from "../Contexts/UserContext";
+import LoginGoogle from "./LoginGoogle";
 
 export default function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
 
@@ -18,6 +19,11 @@ export default function LoginForm() {
   function handleLogin(e) {
     e.preventDefault();
     setLoading(true);
+    if (email == "" || password == "") {
+      setLoading(false);
+      setError("Please enter an email and password");
+      return;
+    }
     return signInWithEmailAndPassword(auth, email, password)
       .then((data) => {
         return getUser(data.user.uid);
@@ -53,27 +59,36 @@ export default function LoginForm() {
   }
 
   return (
-    <form className="form">
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        id="email"
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-        value={email}
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        id="password"
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-        value={password}
-      />
-      {error != "" ? <p className="error">{error}</p> : null}
-      <button onClick={handleLogin}>Login</button>
-    </form>
+    <>
+      <form className="login-form">
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          value={email}
+          className="login-input"
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          value={password}
+          className="login-input"
+        />
+        {error != "" ? <p className="error">{error}</p> : <p></p>}
+        <button onClick={handleLogin} className="login-button">
+          Login
+        </button>
+        <p style={{ textAlign: "center" }}>OR</p>
+      </form>
+      <LoginGoogle />
+    </>
   );
 }
