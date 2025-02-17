85b442a101058fa44f2fca19a8fc3ba087e38f34
Lian Tattersall <lianwan468@gmail.com>
2025-02-17 15:54:30 +0000

    add styling to event fields

--- src/Components/EventField.jsx
+++ src/Components/EventField.jsx
@@ -1,19 +1,24 @@
 export default function EventField({ type, id, setFormData, value, label }) {
   return (
-    <div>
-      <label htmlFor={id}>{label}</label>
-      <input
-        type={type}
-        id={id}
-        onChange={(e) => {
-          setFormData((curr) => {
-            const newFormData = { ...curr };
-            newFormData[id] = e.target.value;
-            return newFormData;
-          });
-        }}
-        value={value}
-      />
-    </div>
+    <>
+      <label htmlFor={id} className="bold" style={{ paddingLeft: "5px" }}>
+        {label}
+      </label>
+      <div style={{ padding: "5px" }}>
+        <input
+          className="event-input"
+          type={type}
+          id={id}
+          onChange={(e) => {
+            setFormData((curr) => {
+              const newFormData = { ...curr };
+              newFormData[id] = e.target.value;
+              return newFormData;
+            });
+          }}
+          value={value}
+        />
+      </div>
+    </>
   );
 }
