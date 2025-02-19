4eef077c9b9ffbedbcc64a4f51db07906d0f0e7f
Lian Tattersall <lianwan468@gmail.com>
2025-02-19 12:36:11 +0000

    create a footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <p>footer</p>
+      <p>Home</p>
+      <p>Browse</p>
+      <p>Copyright</p>
+    </div>
+  );
+}
