3aba5f38883f13ab492643fa62999998441e06ae
Lian Tattersall <lianwan468@gmail.com>
2025-02-19 12:35:50 +0000

    add error messages

--- src/Components/EventField.jsx
+++ src/Components/EventField.jsx
@@ -1,12 +1,24 @@
-export default function EventField({ type, id, setFormData, value, label }) {
+export default function EventField({
+  type,
+  id,
+  setFormData,
+  value,
+  label,
+  error,
+}) {
   return (
     <>
       <label htmlFor={id} className="bold" style={{ paddingLeft: "5px" }}>
         {label}
       </label>
       <div style={{ padding: "5px" }}>
+        {error ? (
+          <p className="error" style={{ fontSize: "13px" }}>
+            Please enter {label.toLowerCase()}
+          </p>
+        ) : null}
         <input
-          className="event-input"
+          className={`event-input ${error ? "red-border" : ""}`}
           type={type}
           id={id}
           onChange={(e) => {
