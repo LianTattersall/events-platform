1d14b55ff01a73c9c400d4c54ae81dd0274d07bc
Lian Tattersall <lianwan468@gmail.com>
2025-02-20 21:08:49 +0000

    create a skip to main component

--- src/Components/SkipToMain.jsx
+++ src/Components/SkipToMain.jsx
@@ -1,1 +1,7 @@
-export default function SkipToMain({ id }) {}
+export default function SkipToMain() {
+  return (
+    <a href={"#main"} className="skip-to-main">
+      Skip to main
+    </a>
+  );
+}
