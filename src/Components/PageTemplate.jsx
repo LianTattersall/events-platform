cb191ac31cc3d8a2bcfe637a58c3d661c36d74e4
Lian Tattersall <lianwan468@gmail.com>
2025-02-20 21:07:15 +0000

    add id main to page template for skip to main button

--- src/Components/PageTemplate.jsx
+++ src/Components/PageTemplate.jsx
@@ -5,16 +5,17 @@ import Footer from "./Footer";
 export default function PageTemplate({ children }) {
   const { menuDrawerOpen, setMenuDrawerOpen } = useContext(MenuDrawerContext);
   return (
-    <div
+    <main
       className={menuDrawerOpen ? "margin-with-drawer" : "margin-no-drawer"}
       onClick={() => {
         setMenuDrawerOpen(false);
       }}
       style={{ display: "flex", flexDirection: "column" }}
+      id="main"
     >
       <div>{children}</div>
       <div style={{ flex: 1 }}></div>
       <Footer />
-    </div>
+    </main>
   );
 }
